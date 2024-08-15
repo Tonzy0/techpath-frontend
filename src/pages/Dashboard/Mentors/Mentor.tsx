13fe18689de2c8001db85cff04718d01c5a83236
Omolade Sunday <38852467+Zechariah-dev@users.noreply.github.com>
2024-08-15 05:21:36 +0100

    fx build

--- src/pages/Dashboard/Mentors/Mentor.tsx
+++ src/pages/Dashboard/Mentors/Mentor.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "@/lib/axios";
 import { User } from "@/types";
 import { useParams } from "react-router-dom";
