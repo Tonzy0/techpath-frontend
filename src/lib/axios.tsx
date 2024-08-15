4db9dc15a74e639801b3f93de9e2ffb3c859d77b
Omolade Sunday <38852467+Zechariah-dev@users.noreply.github.com>
2024-08-15 09:32:06 +0100

    fix: api base url

--- src/lib/axios.tsx
+++ src/lib/axios.tsx
@@ -2,8 +2,8 @@ import axios from "axios";
 const tokens = JSON.parse(localStorage.getItem("tokens") as string);
 
 const axiosInstance = axios.create({
-  // baseURL: "https://techpath-backend.onrender.com",
-  baseURL: "http://localhost:8080",
+  baseURL: "https://techpath-backend.onrender.com",
+  // baseURL: "http://localhost:8080",
   headers: {
     Authorization: tokens ? `Bearer ${tokens.accessToken}` : "",
   },
